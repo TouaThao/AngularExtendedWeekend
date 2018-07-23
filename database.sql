c8fd4bb19a8e4f88d8b8fb08eb718836294f1355
Toua Thao <touathao09@gmail.com>
2018-07-23 09:03:04 -0500

    fix database.sql

--- database.sql
+++ database.sql
@@ -1,5 +1,16 @@
 CREATE DATABASE "pethotel";
 
+CREATE TABLE "pets" (
+  "id" SERIAL PRIMARY KEY,
+  "name" VARCHAR(80),
+  "color" VARCHAR(80),
+  "breed" VARCHAR(80),
+  "age" INTEGER,
+  "image_path" VARCHAR(200),
+  "owner_id" INT references "owner" (id) ON DELETE CASCADE NOT NULL,
+  "checkIn" BOOLEAN DEFAULT FALSE
+	);
+
 CREATE TABLE "owner"(
 	"id" SERIAL PRIMARY KEY,
 	"firstName" VARCHAR(80),
@@ -8,18 +19,8 @@ CREATE TABLE "owner"(
 	"image_path" VARCHAR(200)
 	);
 
-CREATE TABLE "pets" (
-  "id" SERIAL PRIMARY KEY,
-  "name" VARCHAR(80),
-  "color" VARCHAR(80),
-  "breed" VARCHAR(80),
-  "age" INTEGER,
-  "image_path" VARCHAR(200),
-  "checkIn" BOOLEAN DEFAULT FALSE
-	);
-
-INSERT INTO "pets"("name","color","breed","age","checkIn")
-VALUES ('Poky-Dot','White and Black' ,'dalmatian', '1', 'true');
+INSERT INTO "pets"("name","color","breed","age","image_path","owner_id","checkIn")
+VALUES ('Poky-Dot','White and Black', 'dalmatian', '1','dog.jpeg','1','false');
 
 INSERT INTO "owner"("firstName","lastName","city","image_path")
 VALUES ('Roger','Radcliffe','London','Roger_Radcliffe.png');
